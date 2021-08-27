043c5752999240978f97d0c7cf519b31778e4f17
iago12312 <89616042+iago12312@users.noreply.github.com>
2021-08-26 21:11:44 -0300

    Update colab_exe.sh

--- colab_exe.sh
+++ colab_exe.sh
@@ -84,52 +84,6 @@ printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
 { printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
 
-
-
-# Install Google Chrome
-printf "$g$b    Installing Google Chrome $endc$enda" >&2
-{
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg --install google-chrome-stable_current_amd64.deb
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-
-
-# Install CrossOver (Run exe on linux)
-printf "$g$b    Installing CrossOver $endc$enda" >&2
-{
-    wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.4-1.deb
-    sudo dpkg -i crossover_20.0.4-1.deb
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    CrossOver Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-
-# Install VLC Media Player 
-printf "$g$b    Installing VLC Media Player $endc$enda" >&2
-{
-    sudo apt install vlc -y
-} &> /dev/null &&
-printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install other tools like nano
-sudo apt-get install gdebi -y &> /dev/null
-sudo apt-get install vim -y &> /dev/null
-printf "$g$b    Installing other Tools $endc$enda" >&2
-if sudo apt install nautilus nano -y &> /dev/null
-then
-    printf "\r$c$b    Other Tools Installed $endc$enda\n" >&2
-else
-    printf "\r$r$b    Error Occured $endc$enda\n" >&2
-fi
-
-
-
 printf "\n$g$b    Installation Completed $endc$enda\n\n" >&2
 
 
